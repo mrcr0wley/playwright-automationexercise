d6f18eda4a4ae07462b34eebbccf6463f0c309b9
Bohdan Volynets <bohdan_volynets@epam.com>
2025-07-04 19:22:16 +0300

    added test Login with valid credentials

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -8,3 +8,12 @@ test('Login with invalid credentials shows error', async ({ page }) => {
   await page.click('button[data-qa="login-button"]');
   await expect(page.locator('.login-form p')).toContainText('Your email or password is incorrect!');
 });
+
+test('Login with valid credentials', async ({ page }) => {
+  await page.goto('https://automationexercise.com/');
+  await page.click('a[href="/login"]');
+  await page.fill('input[data-qa="login-email"]', 'mr.crowleyit@gmail.com');
+  await page.fill('input[data-qa="login-password"]', 'qwerty');
+  await page.click('button[data-qa="login-button"]');
+  await expect(page.getByText('Logged in as')).toBeVisible();
+});
